dd3cb2bf3069bba21dc48314ed85901d321a6a54
zz312 <zz312@duke.edu>
2023-02-20 20:00:12 -0500

    done

--- http_proxy.cpp
+++ http_proxy.cpp
@@ -14,7 +14,6 @@ int main() {
 
         char msg[65536] = {0} ;
         recv(new_socket, msg, sizeof(msg), 0);
-        cout << msg << endl;
         cout << "hello" << endl;
         cout << "test 2 " << endl;
 
