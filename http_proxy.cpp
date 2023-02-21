2b2e81f512aafbd9ef138e69245f1b3bda6bca58
James Huang <sh694@duke.edu>
2023-02-21 13:14:43 -0500

    01

--- http_proxy.cpp
+++ http_proxy.cpp
@@ -1,37 +1,44 @@
 #include "helper.h"
-#include <sstream>
-
-
+#include "client_request.h"
 
 std::ofstream logFile("proxy.log");
+
+
+
+void handle_connection(int socket_fd) {
+    char msg[65536] = {0};
+    recv(socket_fd, msg, sizeof(msg), 0);
+
+    // Parse the request and store the information in a ClientRequest object
+    ClientRequest request;
+    parse_request(msg, request.method, request.hostname, request.port, request.first_line);
+
+    std::cout << "First line: " << request.first_line << std::endl;
+    std::cout << "Method: " << request.method << std::endl;
+    std::cout << "Hostname: " << request.hostname << std::endl;
+    std::cout << "Port: " << request.port << std::endl;
+    std::cout << "================================" << std::endl;
+
+    // // Pass the ClientRequest object to the request handling and cache management functions
+    // handle_request(request);
+    // manage_cache(request);
+
+}
+
 int main() { 
     int proxy_server_fd = create_server("8080");
-
     int request_id = 0;
     char host[200];
     gethostname(host,sizeof host);
     cout<<host<<endl;
 
 
-    while(true) {
+    while (true) {
         int new_socket = accept_server(proxy_server_fd);
 
-        char msg[65536] = {0} ;
-        recv(new_socket, msg, sizeof(msg), 0);
-
-        std::string method, hostname, first_line;
-        int port;
-        parse_request(msg, method, hostname, port, first_line);
-
-        std::cout << "First line: " << first_line << std::endl;
-        std::cout << "Method: " << method << std::endl;
-        std::cout << "Hostname: " << hostname << std::endl;
-        std::cout << "Port: " << port << std::endl;
-        std::cout << "================================" << std::endl;
-
-        close(new_socket);
-        //break;
-
+        // Create a new thread to handle the incoming request
+        std::thread th(handle_connection, new_socket);
+        th.detach();
     }
     return 0;
 }
