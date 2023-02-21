19e5894f10a525966cf2abfb9843bd020c54b88f
James Huang <sh694@duke.edu>
2023-02-20 20:39:25 -0500

    parse

--- http_proxy.cpp
+++ http_proxy.cpp
@@ -1,5 +1,15 @@
 #include "helper.h"
+#include <sstream>
 
+
+
+void parse_request(const char* msg, std::string& method, std::string& path, std::string& version) {
+    std::istringstream iss(msg);
+    std::getline(iss, method, ' ');
+    std::getline(iss, path, ' ');
+    std::getline(iss, version, '\r');
+    iss.ignore(1);
+}
 
 std::ofstream logFile("proxy.log");
 int main() { 
@@ -9,14 +19,16 @@ int main() {
     char host[200];
     gethostname(host,sizeof host);
     cout<<host<<endl;
+
+
     while(true) {
         int new_socket = accept_server(proxy_server_fd);
 
         char msg[65536] = {0} ;
         recv(new_socket, msg, sizeof(msg), 0);
-        cout << msg << endl;
-        cout << "hello" << endl;
-        cout << "test 2 " << endl;
+        std::string method, path, version;
+        parse_request(msg, method, path, version);
+        cout << method << endl << path<< endl<< version<<endl;
 
     }
     return 0;
